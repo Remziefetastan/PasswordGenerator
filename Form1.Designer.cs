fd49bc988a4cf49da070c04a566f339bb4cfe16a
Remziefetastan <remziefetast123@gmail.com>
2024-10-13 13:18:34 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Generate = new System.Windows.Forms.Button();
+            this.checkBoxNumbers = new System.Windows.Forms.CheckBox();
+            this.checkBoxSpecial = new System.Windows.Forms.CheckBox();
+            this.checkBoxUppercase = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBoxLowercase = new System.Windows.Forms.CheckBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Btn_Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Txt_Password
+            // 
+            this.Txt_Password.Location = new System.Drawing.Point(213, 82);
+            this.Txt_Password.Name = "Txt_Password";
+            this.Txt_Password.Size = new System.Drawing.Size(158, 31);
+            this.Txt_Password.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(89, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(413, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password Generator";
+            // 
+            // Btn_Generate
+            // 
+            this.Btn_Generate.Location = new System.Drawing.Point(122, 201);
+            this.Btn_Generate.Name = "Btn_Generate";
+            this.Btn_Generate.Size = new System.Drawing.Size(158, 32);
+            this.Btn_Generate.TabIndex = 2;
+            this.Btn_Generate.Text = "Generate";
+            this.Btn_Generate.UseVisualStyleBackColor = true;
+            this.Btn_Generate.Click += new System.EventHandler(this.Btn_Generate_Click);
+            // 
+            // checkBoxNumbers
+            // 
+            this.checkBoxNumbers.AutoSize = true;
+            this.checkBoxNumbers.Location = new System.Drawing.Point(21, 131);
+            this.checkBoxNumbers.Name = "checkBoxNumbers";
+            this.checkBoxNumbers.Size = new System.Drawing.Size(101, 27);
+            this.checkBoxNumbers.TabIndex = 3;
+            this.checkBoxNumbers.Text = "Numbers";
+            this.checkBoxNumbers.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSpecial
+            // 
+            this.checkBoxSpecial.AutoSize = true;
+            this.checkBoxSpecial.Location = new System.Drawing.Point(156, 131);
+            this.checkBoxSpecial.Name = "checkBoxSpecial";
+            this.checkBoxSpecial.Size = new System.Drawing.Size(83, 27);
+            this.checkBoxSpecial.TabIndex = 4;
+            this.checkBoxSpecial.Text = "Special";
+            this.checkBoxSpecial.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxUppercase
+            // 
+            this.checkBoxUppercase.AutoSize = true;
+            this.checkBoxUppercase.Location = new System.Drawing.Point(286, 131);
+            this.checkBoxUppercase.Name = "checkBoxUppercase";
+            this.checkBoxUppercase.Size = new System.Drawing.Size(111, 27);
+            this.checkBoxUppercase.TabIndex = 5;
+            this.checkBoxUppercase.Text = "Uppercase";
+            this.checkBoxUppercase.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(213, 164);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(158, 31);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // checkBoxLowercase
+            // 
+            this.checkBoxLowercase.AutoSize = true;
+            this.checkBoxLowercase.Location = new System.Drawing.Point(413, 131);
+            this.checkBoxLowercase.Name = "checkBoxLowercase";
+            this.checkBoxLowercase.Size = new System.Drawing.Size(112, 27);
+            this.checkBoxLowercase.TabIndex = 7;
+            this.checkBoxLowercase.Text = "Lowercase";
+            this.checkBoxLowercase.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Save
+            // 
+            this.Btn_Save.Location = new System.Drawing.Point(286, 201);
+            this.Btn_Save.Name = "Btn_Save";
+            this.Btn_Save.Size = new System.Drawing.Size(158, 32);
+            this.Btn_Save.TabIndex = 8;
+            this.Btn_Save.Text = "Save";
+            this.Btn_Save.UseVisualStyleBackColor = true;
+            this.Btn_Save.Click += new System.EventHandler(this.Btn_Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(570, 267);
+            this.Controls.Add(this.Btn_Save);
+            this.Controls.Add(this.checkBoxLowercase);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBoxUppercase);
+            this.Controls.Add(this.checkBoxSpecial);
+            this.Controls.Add(this.checkBoxNumbers);
+            this.Controls.Add(this.Btn_Generate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Txt_Password);
+            this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Password Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_Generate;
+        private System.Windows.Forms.CheckBox checkBoxNumbers;
+        private System.Windows.Forms.CheckBox checkBoxSpecial;
+        private System.Windows.Forms.CheckBox checkBoxUppercase;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBoxLowercase;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button Btn_Save;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Generate_Click(object sender, EventArgs e)
+        {
+            int passwordLength = (int)numericUpDown1.Value;
+            bool includeNumbers = checkBoxNumbers.Checked;
+            bool includeSpecial = checkBoxSpecial.Checked;
+            bool includeUppercase = checkBoxUppercase.Checked;
+            bool includeLowercase = checkBoxLowercase.Checked;
+
+            string password = GeneratePassword(passwordLength, includeNumbers, includeSpecial, includeUppercase, includeLowercase);
+            Txt_Password.Text = password;
+        }
+        private string GeneratePassword(int length, bool includeNumbers, bool includeSpecial, bool includeUppercase, bool includeLowercase)
+        {
+            string numbers = "0123456789";
+            string specialChars = "!@#$%^&*()+-*/";
+            string uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+
+            StringBuilder characterSet = new StringBuilder();
+
+            if (includeNumbers) characterSet.Append(numbers);
+            if (includeSpecial) characterSet.Append(specialChars);
+            if (includeUppercase) characterSet.Append(uppercaseChars);
+            if (includeLowercase) characterSet.Append(lowercaseChars);
+
+            if (characterSet.Length == 0) return string.Empty;
+
+            Random random = new Random();
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                password.Append(characterSet[random.Next(characterSet.Length)]);
+            }
+
+            return password.ToString();
+        }
+
+        private void Btn_Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Metni kaydet";
+            
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                File.WriteAllText(saveFileDialog.FileName, Txt_Password.Text);
+                MessageBox.Show("Dosya başarıyla kaydedildi.");
+            }
+        }
+    }
+}
